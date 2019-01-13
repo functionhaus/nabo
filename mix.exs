075a625f47108e8d497028313ee77aba79d9423f
Mike Zazaian <mike@functionhaus.com>
2019-01-13 23:33:26 +0100

    update license and mixfile

--- mix.exs
+++ mix.exs
@@ -10,18 +10,23 @@ defmodule Nabo.Mixfile do
      deps: deps(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     organization: "functionhaus",
      description: description(),
      package: package(),
      name: "Nabo",
+     source_url: "https://github.com/functionhaus/nabo",
+     homepage_url: "https://functionhaus.com",
+
      docs: [source_ref: "v#{@version}",
             main: "Nabo",
+            logo: "assets/functionhaus_logo.png",
             canonical: "http://hexdocs.pm/nabo",
             source_url: "https://github.com/functionhaus/nabo"]]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      extra_applications: [:logger]
+    ]
   end
 
   defp deps do
@@ -35,7 +40,9 @@ defmodule Nabo.Mixfile do
   defp package do
     [maintainers: ["Mike Zazaian, Cẩm Huỳnh"],
      licenses: ["Apache 2, MIT"],
-     links: %{"GitHub" => "https://github.com/functionhaus/nabo"},
+     organization: "functionhaus",
+     links: %{"GitHub" => "https://github.com/functionhaus/nabo",
+              "Docs" => "https://hexdocs.pm/conn_artist/"},
      files: ~w(mix.exs README.md lib)]
   end
 
