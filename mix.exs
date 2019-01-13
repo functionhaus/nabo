f98f545ecd4073df788d5c199d984f0a8df8de0b
Mike Zazaian <mike@functionhaus.com>
2019-01-13 23:24:39 +0100

    bump jason and earmark, user elixir 1.7

--- mix.exs
+++ mix.exs
@@ -1,22 +1,23 @@
 defmodule Nabo.Mixfile do
   use Mix.Project
 
-  @version "1.0.2"
+  @version "1.0.3"
 
   def project do
     [app: :nabo,
      version: @version,
-     elixir: "~> 1.4",
+     elixir: "~> 1.7",
      deps: deps(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     organization: "functionhaus",
      description: description(),
      package: package(),
      name: "Nabo",
      docs: [source_ref: "v#{@version}",
             main: "Nabo",
             canonical: "http://hexdocs.pm/nabo",
-            source_url: "https://github.com/qcam/nabo"]]
+            source_url: "https://github.com/functionhaus/nabo"]]
   end
 
   def application do
@@ -25,16 +26,16 @@ defmodule Nabo.Mixfile do
 
   defp deps do
     [
-      {:earmark, "~> 1.2.3", optional: true},
-      {:jason, "~> 1.0.0", optional: true},
-      {:ex_doc, "~> 0.19", only: :dev},
+      {:earmark, "~> 1.3", optional: true},
+      {:jason, "~> 1.1", optional: true},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [maintainers: ["Cẩm Huỳnh"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/qcam/nabo"},
+     licenses: ["Apache 2"],
+     links: %{"GitHub" => "https://github.com/functionhaus/nabo"},
      files: ~w(mix.exs README.md lib)]
   end
 
